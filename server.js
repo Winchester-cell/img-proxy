e076c5b178500dc8b83a994da98d8fa8ab74d1e6
Winchester-cell <manili.mm91@gmail.com>
2025-02-18 11:38:35 +0330

    Added Sharp for image compression

--- server.js
+++ server.js
@@ -1,28 +1,39 @@
 const express = require('express');
 const axios = require('axios');
+const sharp = require('sharp');
+
 const app = express();
 
 app.get('/image-proxy', async (req, res) => {
   const imageUrl = req.query.url;
+  const quality = parseInt(req.query.quality) || 80; 
+
   if (!imageUrl) {
     return res.status(400).send('Image URL is required');
   }
 
   try {
     const response = await axios.get(imageUrl, {
-      responseType: 'stream',
+      responseType: 'arraybuffer',
       headers: {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
-        'Referer': 'https://mangadex.org/',  // وانمود می‌کنیم که درخواست از خود مانگادکس اومده
+        'Referer': 'https://mangadex.org/',
         'Origin': 'https://mangadex.org'
       }
     });
 
-    res.set('Content-Type', response.headers['content-type']);
-    response.data.pipe(res);
+    const buffer = Buffer.from(response.data);
+    
+   
+    const compressedImage = await sharp(buffer)
+      .jpeg({ quality }) 
+      .toBuffer();
+
+    res.set('Content-Type', 'image/jpeg');
+    res.send(compressedImage);
   } catch (error) {
-    console.error('Error fetching image:', error.message);
-    res.status(500).send('Error fetching image.');
+    console.error('Error fetching or processing image:', error.message);
+    res.status(500).send('Error fetching or processing image.');
   }
 });
 
